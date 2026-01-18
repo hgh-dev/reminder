1968ce1b9ada89e299dc5654ca679accd0c54f2f
hussell <hussell.app@gmail.com>
2026-01-18 11:42:38 +0900

    첫 업로드

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'reminder-v4-0-0';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
